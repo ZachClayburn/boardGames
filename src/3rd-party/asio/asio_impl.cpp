166837e7ab4111164ae9b0b44ee69955cb15d28b
Zach Clayburn <zachclayburn@gmail.com>
2021-04-09 14:20:17 -0600

    tableing this for now

---
+++ src/3rd-party/asio/asio_impl.cpp
@@ -0,0 +1,4 @@
+#ifndef ASIO_STANDALONE
+#error "ASIO should be in standalone mode!"
+#endif
+#include <asio/impl/src.hpp>
---
+++ src/common/network/Serializer.h
@@ -0,0 +1,15 @@
+#ifndef MYPROJECT_SERIALIZER_H
+#define MYPROJECT_SERIALIZER_H
+
+#include <iostream>
+
+namespace boardgames::network::serialize {
+
+
+class Serializer
+{
+};
+
+}// namespace boardgames::network::serialize
+
+#endif//MYPROJECT_SERIALIZER_H
